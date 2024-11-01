27adaf32964a5d9ff2e0ce2240cde4b0cb17ac2b
Phil Jones <interstar@gmail.com>
2024-11-01 00:00:58 +0000

    extra for fl studio

--- current/haxe/makeall.sh
+++ current/haxe/makeall.sh
@@ -2,4 +2,5 @@ haxe py-lib.hxml
 haxe js-site.hxml
 
 cp out/python/goldenpond.py for-distribution/pypi/goldenpond/goldenpond.py
+cp out/python/goldenpond.py for-distribution/fl/generated.py
 cp out/js/goldenpond.js for-distribution/web-app/goldenpond.js
