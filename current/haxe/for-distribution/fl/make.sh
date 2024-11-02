fd2023c482894c2319b2ac9a3697ac6f3d67ec2e
Phil Jones <interstar@gmail.com>
2024-11-02 17:47:24 +0000

    Moving rebuilt script to 'published' directory

--- current/haxe/for-distribution/fl/make.sh
+++ current/haxe/for-distribution/fl/make.sh
@@ -4,6 +4,8 @@ cp ../../out/python/goldenpond.py generated.py
 
 cat pre.py generated.py post.py > goldenpond.pyscript
 
+cp goldenpond.pyscript  ../../../published/
+
 echo "Now put the goldenpond.pyscript wherever FL Studio can find it. Probably  <User>/YOURNAME/Documents/Image-Line/FL Studio/Settings/Piano roll scripts/goldenpond.pyscript"
 
 
